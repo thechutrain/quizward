21807df41dfafd2d4a066f1980429a8fb2243032
thechutrain <chu.chuyalan@gmail.com>
2017-03-08 11:32:11 -0500

    added public, views, controller folders. Npm's

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const path = require('path');
+var exphbs = require('express-handlebars');
+
+// lib
 var Models = require('./models');
+var defaultController = require('./controllers/defaultController.js')
 
 // Create app
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Set up view w. Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(session({
+  secret: 'app',
+  cookie: { maxAge: 6 * 1000 * 1000 * 1000 * 1000 },
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(process.cwd(), '/public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
 // Routers
+app.use('/', defaultController);
 
 // Create Server
 Models.sequelize.sync().then(function(){
